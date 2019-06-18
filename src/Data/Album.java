4f059d4402ab2bf4c4599aaf6a147f2ed2bfcbc1
fatemeh <fatemeh.vpasha@gmail.com>
2019-06-18 13:32:18 +0430

    User & Album class in Data package are initialized.

--- src/Data/Album.java
+++ src/Data/Album.java
@@ -1,4 +1,14 @@
 package Data;
 
+import java.util.ArrayList;
+
 public class Album {
+    private ArrayList<Music> musics;
+    private static ArrayList<Album> albums  = new ArrayList<>();
+
+    public Album() {
+        musics = new ArrayList<>();
+        albums.add(this);
+    }
+    //TODO complete today
 }
--- src/Data/User.java
+++ src/Data/User.java
@@ -1,4 +1,15 @@
 package Data;
 
 public class User {
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
 }
