71e7ceb4d0f4d39f58d2392843f4bc38fa59f2fc
fatemeh <fatemeh.vpasha@gmail.com>
2019-06-19 09:08:09 +0430

    completes Album class.

--- src/Data/Album.java
+++ src/Data/Album.java
@@ -3,12 +3,33 @@ package Data;
 import java.util.ArrayList;
 
 public class Album {
+    private String name;
     private ArrayList<Music> musics;
     private static ArrayList<Album> albums  = new ArrayList<>();
 
-    public Album() {
+    public Album(String name) {
+        this.name = name;
         musics = new ArrayList<>();
         albums.add(this);
+    }
+
+    private void addMusic(Music music){
+        musics.add(music);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void addMusicToAlbum(Music music){
+        for (Album album : albums){
+            if (album.getName().equals(music.getAlbum())){
+                album.addMusic(music);
+                return;
+            }
+        }
+        Album album = new Album("" + music.getAlbum());
+        album.addMusic(music);
     }
 
 }
