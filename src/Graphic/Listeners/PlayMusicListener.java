62ff6c62b00f4b8ba9828029a565f517c0e53d49
fatemeh <fatemeh.vpasha@gmail.com>
2019-06-19 13:20:11 +0430

    Adds some prototype of classes in Listeners package.

---
+++ src/Graphic/Listeners/PlayMusicListener.java
@@ -0,0 +1,31 @@
+package Graphic.Listeners;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class PlayMusicListener implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
