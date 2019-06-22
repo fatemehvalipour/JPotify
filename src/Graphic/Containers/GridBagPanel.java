fefa982028efcbe2ad5445bdead7acf99afb7bf3
fatemeh <fatemeh.vpasha@gmail.com>
2019-06-22 15:51:37 +0430

    adds some icon.

--- src/Graphic/Containers/GridBagPanel.java
+++ src/Graphic/Containers/GridBagPanel.java
@@ -1,5 +1,6 @@
 package Graphic.Containers;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
@@ -12,8 +13,32 @@ public class GridBagPanel extends JPanel {
 
     public GridBagPanel(GridPanel gridPanel) {
         super();
-        gridPanel.add(this);
-        setBackground(Color.black);
+        playButton = new JButton();
+        nextButton = new JButton();
+        previousButton = new JButton();
+        likeButton = new JButton();
+        shuffleButton = new JButton();
+//        try {
+//            playButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("play.png"))));
+//            playButton.setSize(new Dimension(1000000, 100000));
+//            nextButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("next.png"))));
+//            previousButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("previous.png"))));
+//            likeButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("heartBlue.png"))));
+//            shuffleButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("shuffle.png"))));
+//
+//        } catch (Exception ex) {
+//            System.out.println(ex);
+//        }
+
         setLayout(new GridBagLayout());
+        setBackground(Color.black);
+
+        add(likeButton);
+        add(previousButton);
+        add(playButton);
+        add(nextButton);
+        add(shuffleButton);
+        gridPanel.add(this);
+
     }
 }
