f897e83d004c19156fca3c045fb94b51458d1e1f
KoroshRH <46434958+KoroshRH@users.noreply.github.com>
2019-06-24 16:59:13 +0430

    sets save method in backup class.

--- src/Data/Backup.java
+++ src/Data/Backup.java
@@ -1,14 +1,20 @@
 package Data;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class Backup {
     private static ObjectInputStream objectInputStream;
     private static ObjectOutputStream objectOutputStream;
 
     public static void save(){
-
+        try {
+            objectOutputStream = new ObjectOutputStream(new DataOutputStream(new FileOutputStream("save.sv")));
+            objectOutputStream.writeObject(Music.getMusics());
+            objectOutputStream.writeObject(Album.getAlbums());
+            objectOutputStream.writeObject(PlayList.getPlayLists());
+        } catch (IOException e) {
+            System.out.println("can't save the files");
+        }
     }
 
     public static void load(){
